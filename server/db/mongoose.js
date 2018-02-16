e7afb2cd424bfffce542779a2ecaa09b09c99149
Danilo Del Fio <ddelfio@gmail.com>
2018-02-16 11:54:04 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
+// this is a workaround to not insert credit card credentials into heroku
+const REMOTE_MONGO = "mongodb://root:root@ds237858.mlab.com:37858/todoapp";
+const LOCAL_MONGO = "mongodb://localhost:27017/TodoApp";
+const MONGODB_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(MONGODB_URI);
 
 module.exports = {mongoose};
