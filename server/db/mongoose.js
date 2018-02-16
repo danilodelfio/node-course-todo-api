628b840bf94681e285c91608585ef262539e7673
Danilo Del Fio <ddelfio@gmail.com>
2018-02-16 13:33:36 +0100

    MONGODB_URI env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 
 // this is a workaround to not insert credit card credentials into heroku
-const REMOTE_MONGO = "mongodb://root:root@ds237858.mlab.com:37858/todoapp";
+//const REMOTE_MONGO = "mongodb://root:root@ds237858.mlab.com:37858/todoapp";
 const LOCAL_MONGO = "mongodb://localhost:27017/TodoApp";
-const MONGODB_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || LOCAL_MONGO);
 
 module.exports = {mongoose};
